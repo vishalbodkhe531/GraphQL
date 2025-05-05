e0c224aa7c7dbef408095922bb4b507873669d7c
vishalbodkhe531 <vishalbodkhe531@gmail.com>
2025-05-05 22:34:40 +0530

    create Apollo-Server

--- 01_Basics/server/src/app.ts
+++ 01_Basics/server/src/app.ts
@@ -1,76 +1,32 @@
-// import express from "express"
-// import helmet from "helmet"
-// import { connectGraphQL } from "@/graphql/graphql.js"
-// import { expressMiddleware } from "@apollo/server/express4";
-// import cors from 'cors'
-// import { errorMiddleware } from "@/middlewares/error.js"
-// import morgan from "morgan"
-// import dotenv from "dotenv"
-// import { connectDB } from "@/lib/db.js"
-
-//   dotenv.config({path: './.env',});
-
-//   export const envMode = process.env.NODE_ENV?.trim() || 'DEVELOPMENT';
-//   const port = process.env.PORT || 3000;
-
-// // const mongoURI = process.env.MONGO_URI! || 'mongodb://localhost:27017';
-
-// // connectDB(mongoURI);
-
-//   const app = express();
-
-// const graphqlServer = connectGraphQL();
-// await graphqlServer.start();
-
-// app.use(
-//   helmet({
-//     contentSecurityPolicy: envMode !== "DEVELOPMENT",
-//     crossOriginEmbedderPolicy: envMode !== "DEVELOPMENT",
-//   })
-// );
-
-// app.use(express.json());
-// app.use(express.urlencoded({extended: true}));
-// app.use("/graphql", expressMiddleware(graphqlServer));
-// app.use(cors({origin:' * ',credentials:true}));
-// app.use(morgan('dev'))
-
-// app.get('/', (req, res) => {
-//    res.send('Hello, World!');
-// });
-
-//   // your routes here
-
-//   app.get("*", (req, res) => {
-//     res.status(404).json({
-//       success: false,
-//       message: "Page not found",
-//     });
-//   });
-
-//   app.use(errorMiddleware);
-
-//   app.listen(port, () => console.log('Server is working on Port:'+port+' in '+envMode+' Mode.'));
-
 import dotenv from "dotenv";
-import express from "express";
-import morgan from "morgan";
+import { ApolloServer } from "@apollo/server";
+import { startStandaloneServer } from "@apollo/server/standalone";
+
 dotenv.config({ path: "./.env" });
-import cors from "cors";
 
-const port = process.env.PORT || 3000;
+export const envMode = process.env.NODE_ENV?.trim() || "DEVELOPMENT";
 
-const app = express();
+const port = Number(process.env.PORT) || 3000;
 
-app.use(express.json());
-app.use(express.urlencoded({ extended: true }));
-app.use(cors({ origin: " * ", credentials: true }));
-app.use(morgan("dev"));
-
-app.get("/", (req, res) => {
-  res.send("Hello, World!");
+const server = new ApolloServer({
+  typeDefs: `type Query {
+    hello: String
+  }`,
+  resolvers: {
+    Query: {
+      hello: () => "Hello, world!",
+    },
+  },
 });
 
-app.listen(port, () =>
-  console.log(`Server is working on Port : http://localhost:${port}`)
-);
+startStandaloneServer(server, {
+  listen: { port },
+})
+  .then(() => {
+    console.log(
+      "Server is working on Port:" + port + " in " + envMode + " Mode."
+    );
+  })
+  .catch((err) => {
+    console.log(err);
+  });
