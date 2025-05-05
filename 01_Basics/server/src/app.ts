cd86ad3e5192ba2237014f2b7b6daa0265d352ec
vishalbodkhe531 <vishalbodkhe531@gmail.com>
2025-05-05 19:51:40 +0530

    remove temp graphQL things and mongo connection

--- 01_Basics/server/src/app.ts
+++ 01_Basics/server/src/app.ts
@@ -1,60 +1,76 @@
+// import express from "express"
+// import helmet from "helmet"
+// import { connectGraphQL } from "@/graphql/graphql.js"
+// import { expressMiddleware } from "@apollo/server/express4";
+// import cors from 'cors'
+// import { errorMiddleware } from "@/middlewares/error.js"
+// import morgan from "morgan"
+// import dotenv from "dotenv"
+// import { connectDB } from "@/lib/db.js"
 
-import express from "express"
-import helmet from "helmet"
-import { connectGraphQL } from "@/graphql/graphql.js"
-import { expressMiddleware } from "@apollo/server/express4";
-import cors from 'cors'
-import { errorMiddleware } from "@/middlewares/error.js"
-import morgan from "morgan"
-import dotenv from "dotenv"
-import { connectDB } from "@/lib/db.js"
-  
-  dotenv.config({path: './.env',});
-  
-  export const envMode = process.env.NODE_ENV?.trim() || 'DEVELOPMENT';
-  const port = process.env.PORT || 3000;
-  
-const mongoURI = process.env.MONGO_URI! || 'mongodb://localhost:27017';
+//   dotenv.config({path: './.env',});
 
-connectDB(mongoURI);
-  
-  const app = express();
-  
-const graphqlServer = connectGraphQL();
-await graphqlServer.start();
-    
-                                
-  
-  
-app.use(
-  helmet({
-    contentSecurityPolicy: envMode !== "DEVELOPMENT",
-    crossOriginEmbedderPolicy: envMode !== "DEVELOPMENT",
-  })
-);
-    
+//   export const envMode = process.env.NODE_ENV?.trim() || 'DEVELOPMENT';
+//   const port = process.env.PORT || 3000;
+
+// // const mongoURI = process.env.MONGO_URI! || 'mongodb://localhost:27017';
+
+// // connectDB(mongoURI);
+
+//   const app = express();
+
+// const graphqlServer = connectGraphQL();
+// await graphqlServer.start();
+
+// app.use(
+//   helmet({
+//     contentSecurityPolicy: envMode !== "DEVELOPMENT",
+//     crossOriginEmbedderPolicy: envMode !== "DEVELOPMENT",
+//   })
+// );
+
+// app.use(express.json());
+// app.use(express.urlencoded({extended: true}));
+// app.use("/graphql", expressMiddleware(graphqlServer));
+// app.use(cors({origin:' * ',credentials:true}));
+// app.use(morgan('dev'))
+
+// app.get('/', (req, res) => {
+//    res.send('Hello, World!');
+// });
+
+//   // your routes here
+
+//   app.get("*", (req, res) => {
+//     res.status(404).json({
+//       success: false,
+//       message: "Page not found",
+//     });
+//   });
+
+//   app.use(errorMiddleware);
+
+//   app.listen(port, () => console.log('Server is working on Port:'+port+' in '+envMode+' Mode.'));
+
+import dotenv from "dotenv";
+import express from "express";
+import morgan from "morgan";
+dotenv.config({ path: "./.env" });
+import cors from "cors";
+
+const port = process.env.PORT || 3000;
+
+const app = express();
+
 app.use(express.json());
-app.use(express.urlencoded({extended: true}));
-app.use("/graphql", expressMiddleware(graphqlServer));
-app.use(cors({origin:' * ',credentials:true}));
-app.use(morgan('dev'))
-    
-  
-  app.get('/', (req, res) => {
-     res.send('Hello, World!');
-  });
-  
-  // your routes here
-  
-    
-  app.get("*", (req, res) => {
-    res.status(404).json({
-      success: false,
-      message: "Page not found",
-    });
-  });
-  
-  app.use(errorMiddleware);
-    
-  app.listen(port, () => console.log('Server is working on Port:'+port+' in '+envMode+' Mode.'));
-  
+app.use(express.urlencoded({ extended: true }));
+app.use(cors({ origin: " * ", credentials: true }));
+app.use(morgan("dev"));
+
+app.get("/", (req, res) => {
+  res.send("Hello, World!");
+});
+
+app.listen(port, () =>
+  console.log(`Server is working on Port : http://localhost:${port}`)
+);
